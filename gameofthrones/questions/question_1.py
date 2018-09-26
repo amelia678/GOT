acfea96c5f4cc3e95dd8356626cc98d2bb0751de
Amelia Schulz email = amelia.schulz@gmail.com <ameliaschulz@Amelias-MacBook-Pro.local>
2018-09-26 10:57:01 -0400

    adds function

--- gameofthrones/questions/question_1.py
+++ gameofthrones/questions/question_1.py
@@ -1,12 +1,17 @@
 from  characters import *
-a_names = []
-for key in range(len(characters)):
-    if characters[key]['name'][0] == 'A':
-        a_names.append(characters[key]['name'])
+
+def how_many_starts_with(letter):
+    names = []
+    for key in range(len(characters)): #for person in characters
+        if characters[key]['name'][0] == letter: #person['name'][0]
+            names.append(characters[key]['name'])
+    print(len(names))  
+
+how_many_starts_with('A')
+how_many_starts_with('Z')
 
 # print(a_names) 
-print(len(a_names))       
 
 # print(characters[0])
 
-# {'url': 'https://www.anapioficeandfire.com/api/characters/2', 'name': 'Walder', 'gender': 'Male', 'culture': '', 'born': '', 'died': '', 'titles': [''], 'aliases': ['Hodor'], 'father': '', 'mother': '', 'spouse': '', 'allegiances': ['https://www.anapioficeandfire.com/api/houses/362'], 'books': ['https://www.anapioficeandfire.com/api/books/1', 'https://www.anapioficeandfire.com/api/books/2', 'https://www.anapioficeandfire.com/api/books/3', 'https://www.anapioficeandfire.com/api/books/5', 'https://www.anapioficeandfire.com/api/books/8'], 'povBooks': [], 'tvSeries': ['Season 1', 'Season 2', 'Season 3', 'Season 4', 'Season 6'], 'playedBy': ['Kristian Nairn']}
+# {'url': 'https://www.anapioficeandfire.com/api/characters/2', 'name': 'Walder', 'gender': 'Male', 'culture': '', 'born': '', 'died': '', 'titles': [''], 'aliases': ['Hodor'], 'father': '', 'mother': '', 'spouse': '', 'allegiances': ['https://www.anapioficeandfire.com/api/houses/362'],  'books': ['https://www.anapioficeandfire.com/api/books/1', 'https://www.anapioficeandfire.com/api/books/2', 'https://www.anapioficeandfire.com/api/books/3', 'https://www.anapioficeandfire.com/api/books/5', 'https://www.anapioficeandfire.com/api/books/8'], 'povBooks': [], 'tvSeries': ['Season 1', 'Season 2', 'Season 3', 'Season 4', 'Season 6'], 'playedBy': ['Kristian Nairn']}
